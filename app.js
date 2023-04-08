7ff7637f3525a5d36a173603f073c6fee17a1540
ced_stark08 <cedricngangue08@gmail.com>
2023-04-08 16:42:35 +0200

    first commit test 5

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const createError = require('http-errors');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -15,12 +16,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(bodyParser.json());
 app.use('/uploads/documents', express.static('./uploads/documents'));
 app.use('/uploads/images', express.static('./uploads/images'));
+app.use((req, res, next) => {
+    res.setHeader('Access-Controll-Allow-Origin', '*')
+    res.setHeader('Access-Controll-Allow-Headers', 'Origin,X-requested-with,Content,Accept,Content-type')
+    res.setHeader('Access-Controll-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader('Access-Controll-Allow-Origin', '*')
+    next()
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
