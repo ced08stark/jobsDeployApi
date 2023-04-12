dad24663da1323a78d70e7e87b186cb9e8aed5f1
ced_stark08 <cedricngangue08@gmail.com>
2023-04-12 07:04:50 +0200

    commit update add getAlljobs test async

--- models/index.js
+++ models/index.js
@@ -9,14 +9,14 @@ const env = "development";
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-//const sequelize = new Sequelize(`${config.url}?sslmode=no-verify`);
+const sequelize = new Sequelize(`${config.url}?sslmode=no-verify`);
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 fs
   .readdirSync(__dirname)
